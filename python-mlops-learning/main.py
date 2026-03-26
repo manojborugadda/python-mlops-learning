45502911f46dabe7d742a16c3fd96122392414b7
Manoj Borugadda <borugadda.manoj@gmail.com>
2026-03-26 12:58:45 +0530

    added indetifiers

---
+++ python-mlops-learning/main.py
@@ -0,0 +1,10 @@
+my_variable = "Hello, MLOps!"
+print(my_variable)
+My_variable = "Welcome to Python MLOps learning!"
+print(My_variable)
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(5, 3)
+print(f"The result of adding 5 and 3 is: {result}")
