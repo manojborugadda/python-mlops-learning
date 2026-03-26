3cc4561a12d60ea5ee39bc9dc392a1bdd1a384a9
Manoj Borugadda <borugadda.manoj@gmail.com>
2026-03-26 23:45:20 +0530

    keywords

---
+++ keywords/main.py
@@ -0,0 +1,19 @@
+# This code will raise a SyntaxError because 'if' and 'def' are reserved keywords in Python and cannot be used as variable names.
+
+"""
+
+if = 10
+print(if)
+
+def = 20
+print(def)
+
+"""
+
+#####################
+# The correct way to use variables is to choose names that are not reserved keywords.
+a = 5
+print(a)
+
+b = 10
+print(b)
